fe4b6dd2d6e98ae802e54a707e6513551d0d7a21
LV9652 <lyujiahe@mail.utoronto.ca>
2019-09-13 23:46:06 -0400

    ji

--- ai_competition.py
+++ ai_competition.py
@@ -7,7 +7,14 @@ if __name__ == "__main__":
     Player_zf = DummyAI(2, "White")
 
     Player_black = Player(1, "Black")
-    Player_white = Player(1, "White")
+    Player_white = Player(2, "White")
 
+    # Can choose different modes
     game = Game(Board(15, 15), Player_lv, Player_zf)
+    # game = Game(Board(15, 15), Player_zf, Player_lv)
+    # game = Game(Board(15, 15), Player_lv, Player_white)
+    # game = Game(Board(15, 15), Player_black, Player_lv)
+    # game = Game(Board(15, 15), Player_zf, Player_black)
+    # game = Game(Board(15, 15), Player_black, Player_zf)
+
     game.play_one_round()
