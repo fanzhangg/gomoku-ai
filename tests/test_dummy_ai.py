1c1bcf8b1ddf241812ecf666799487ab94df5e4e
fanzhangg <vanadiumzhang@gmail.com>
2019-09-14 22:18:27 -0500

    Implement defend living three

--- tests/test_dummy_ai.py
+++ tests/test_dummy_ai.py
@@ -1,11 +1,11 @@
-from dummy_ai import DummyAI, Chain
-from go import Board, Player
+from dummy_ai import DummyAI
+from go import Board
 import unittest
 
 
 class TestGetMaxChainAtStep(unittest.TestCase):
     def setUp(self):
-        self.ai = DummyAI(1, "Black")
+        self.ai = DummyAI(2, 1, "Black")
 
     def test_continuous_chain(self):
         board = Board(4, 4)
@@ -29,7 +29,12 @@ class TestGetMaxChainAtStep(unittest.TestCase):
         self.assertEqual(2, chain.size)
         self.assertEqual((0, 2), chain.gap)
 
-    def test_get_unblocked_eyes(self):
+
+class TestGetDefendingPoint(unittest.TestCase):
+    def setUp(self):
+        self.ai = DummyAI(2, 1, "Black")
+
+    def test_get_1_unblocked_eyes(self):
         board = Board(1, 5)
         board.board = [[2, 1, 1, 1, 0]]
 
@@ -37,241 +42,35 @@ class TestGetMaxChainAtStep(unittest.TestCase):
         eyes = self.ai.get_unblocked_eyes(board, chain)
         self.assertEqual([(0, 4)], eyes)
 
+        board = Board(4, 4)
+        board.board = [
+            [0, 0, 0, 0],
+            [1, 1, 1, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        chain = self.ai.get_max_chain_at_step(board, (1, 1), (0, 1))
+        eyes = self.ai.get_unblocked_eyes(board, chain)
+        self.assertEqual([(1, 3)], eyes)
+
+    def test_get_2_unblocked_eyes(self):
+        board = Board(4, 4)
+        board.board = [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        chain = self.ai.get_max_chain_at_step(board, (1, 1), (0, 1))
+        eyes = self.ai.get_unblocked_eyes(board, chain)
+        self.assertEqual([(1, 3), (1, 0)], eyes)
+
         board = Board(1, 5)
         board.board = [[0, 1, 1, 1, 0]]
 
         chain = self.ai.get_max_chain_at_step(board, (0, 1), (0, 1))
         eyes = self.ai.get_unblocked_eyes(board, chain)
         self.assertEqual([(0, 4), (0, 0)], eyes)
-
-
-# class TestDummyAi(unittest.TestCase):
-#     def test_get_max_chain_stones(self):
-#         # test blocked cases
-#         # - block by the wall
-#         board = Board(4, 4)
-#         board.board = [
-#             [1, 0, 0, 0],
-#             [0, 1, 0, 0],
-#             [0, 0, 1, 0],
-#             [0, 0, 0, 0]]
-#         ai = DummyAI(1, "white")
-#         actual, coord = ai.get_max_chain_at_step(board, (1, 1), (1, 1))
-#         self.assertEqual(0, actual)
-#
-#         actual, coord = ai.get_max_chain_at_step(board, (0, 0), (1, 1))
-#         self.assertEqual(0, actual)
-#
-#         actual, coord = ai.get_max_chain_at_step(board, (2, 2), (1, 1))
-#         self.assertEqual(0, actual)
-#
-#         board = Board(4, 4)
-#         board.board = [
-#             [0, 0, 0, 0],
-#             [0, 1, 0, 0],
-#             [0, 0, 1, 0],
-#             [0, 0, 0, 1]]
-#         ai = DummyAI(1, "white")
-#         actual, coord = ai.get_max_chain_at_step(board, (1, 1), (1, 1))
-#         self.assertEqual(0, actual)
-#
-#         actual, coord = ai.get_max_chain_at_step(board, (0, 0), (1, 1))
-#         self.assertEqual(0, actual)
-#
-#         actual, coord = ai.get_max_chain_at_step(board, (2, 2), (1, 1))
-#         self.assertEqual(0, actual)
-#
-#         # - block by another stone
-#         board = Board(4, 4)
-#         board.board = [
-#             [2, 0, 0, 0],
-#             [0, 1, 0, 0],
-#             [0, 0, 1, 0],
-#             [0, 0, 0, 0]]
-#         ai = DummyAI(1, "white")
-#         actual, coord = ai.get_max_chain_at_step(board, (1, 1), (1, 1))
-#         self.assertEqual(0, actual)
-#
-#         actual, coord = ai.get_max_chain_at_step(board, (0, 0), (1, 1))
-#         self.assertEqual(0, actual)
-#
-#         actual, coord = ai.get_max_chain_at_step(board, (2, 2), (1, 1))
-#         self.assertEqual(0, actual)
-#
-#         board = Board(4, 4)
-#         board.board = [
-#             [0, 0, 0, 0],
-#             [0, 1, 0, 0],
-#             [0, 0, 1, 0],
-#             [0, 0, 0, 2]]
-#         ai = DummyAI(1, "white")
-#         actual, coord = ai.get_max_chain_at_step(board, (1, 1), (1, 1))
-#         self.assertEqual(0, actual)
-#
-#         actual, coord = ai.get_max_chain_at_step(board, (0, 0), (1, 1))
-#         self.assertEqual(0, actual)
-#
-#         actual, coord = ai.get_max_chain_at_step(board, (2, 2), (1, 1))
-#         self.assertEqual(0, actual)
-#
-#         # - test block and with a gap
-#         board2 = Board(6, 6)
-#         board2.board = [
-#             [0, 0, 0, 0, 0, 0],
-#             [0, 1, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 1, 0, 0],
-#             [0, 0, 0, 0, 1, 0],
-#             [0, 0, 0, 0, 0, 2]
-#         ]
-#         actual, coord = ai.get_max_chain_at_step(board2, (1, 1), (1, 1))
-#         self.assertEqual(0, actual)
-#         self.assertEqual(None, coord)
-#
-#         actual, coord = ai.get_max_chain_at_step(board2, (4, 4), (1, 1))
-#         self.assertEqual(0, actual)
-#         self.assertEqual(None, coord)
-#
-#         # test continuous chain
-#         board2.board  = [
-#             [0, 0, 0, 0, 0, 0],
-#             [0, 1, 0, 0, 0, 0],
-#             [0, 0, 1, 0, 0, 0],
-#             [0, 0, 0, 1, 0, 0],
-#             [0, 0, 0, 0, 1, 0],
-#             [0, 0, 0, 0, 0, 0]
-#         ]
-#
-#         actual, coord = ai.get_max_chain_at_step(board2, (2, 2), (1, 1))
-#         self.assertEqual(4, actual)
-#         self.assertEqual(None, coord)
-#
-#         actual, coord = ai.get_max_chain_at_step(board2, (1, 1), (1, 1))
-#         self.assertEqual(4, actual)
-#         self.assertEqual(None, coord)
-#
-#         actual, coord = ai.get_max_chain_at_step(board2, (4, 4), (1, 1))
-#         self.assertEqual(4, actual)
-#         self.assertEqual(None, coord)
-#
-#         # test chain with a gap
-#         board2.board = [
-#             [0, 0, 0, 0, 0, 0],
-#             [0, 1, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 1, 0, 0],
-#             [0, 0, 0, 0, 1, 0],
-#             [0, 0, 0, 0, 0, 0]
-#         ]
-#         actual, coord = ai.get_max_chain_at_step(board2, (1, 1), (1, 1))
-#         self.assertEqual(3, actual)
-#         self.assertEqual((2, 2), coord)
-#
-#         actual, coord = ai.get_max_chain_at_step(board2, (4, 4), (1, 1))
-#         self.assertEqual(3, actual)
-#         self.assertEqual((2, 2), coord)
-#
-#         # test chain with 2 gaps
-#         board3 = Board(7, 7)
-#         board3.board = [
-#             [0, 0, 0, 0, 0, 0, 0],
-#             [0, 1, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 1, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 1, 0],
-#             [0, 0, 0, 0, 0, 0, 0]
-#         ]
-#         actual, coord = ai.get_max_chain_at_step(board3, (1, 1), (1, 1))
-#         self.assertEqual(2, actual)
-#         self.assertEqual((2, 2), coord)
-#
-#         actual, coord = ai.get_max_chain_at_step(board3, (5, 5), (1, 1))
-#         self.assertEqual(2, actual)
-#         self.assertEqual((4, 4), coord)
-
-
-# class TestMaxChainAtPoint(unittest.TestCase):
-#     def setUp(self):
-#         self.ai = DummyAI(1, "Black")
-#         self.board = Board(10, 10)
-#
-#     def test_single_chain(self):
-#         self.board.board = [
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#         ]
-#         point_max, step, gap = self.ai.get_max_chain_at_point(self.board, (1, 1))
-#         self.assertEqual(4, point_max)
-#         self.assertEqual((1, 1), step)
-#         self.assertEqual(None, gap)
-#
-#     def test_multiple_chain(self):
-#         self.board.board = [
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-#             [0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-#             [0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#         ]
-#         point_max, step, gap = self.ai.get_max_chain_at_point(self.board, (2, 2))
-#         self.assertEqual(4, point_max)
-#         self.assertEqual((1, 1), step)
-#         self.assertEqual(None, gap)
-#
-#     def test_blocked_chain(self):
-#         self.board.board = [
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 2, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#         ]
-#         point_max, step, gap = self.ai.get_max_chain_at_point(self.board, (2, 2))
-#         self.assertEqual(2, point_max)
-#         self.assertEqual((0, 1), step)
-#         self.assertEqual(None, gap)
-#
-#     def test_gap_chain(self):
-#         self.board.board = [
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#         ]
-#         point_max, step, gap = self.ai.get_max_chain_at_point(self.board, (2, 2))
-#         self.assertEqual(3, point_max)
-#         self.assertEqual((1, 1), step)
-#         self.assertEqual((3, 3), gap)
-
-
-class TestGetDefendingPoint(unittest.TestCase):
-    def setUp(self):
-        self.ai = DummyAI(1, "Black")
 
     def test_live_four(self):
         board = Board(1, 6)
@@ -295,9 +94,20 @@ class TestGetDefendingPoint(unittest.TestCase):
 
     def test_live_three(self):
         board = Board(1, 5)
-        board.board = [[0, 1, 1, 1, 0]]
+        board.board = [[0, 1, 1, 1, 0, 0, 0, 0]]
         point = self.ai.get_defending_point(board, (0, 1))
         self.assertEqual((0, 4), point)
+
+        board2 = Board(5, 5)
+        board2.board = [
+            [0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0]
+        ]
+        point = self.ai.get_defending_point(board2, (1, 1))
+        self.assertEqual((4, 4), point)
 
     def test_dead_three(self):
         board = Board(1, 5)
@@ -316,3 +126,126 @@ class TestGetDefendingPoint(unittest.TestCase):
         board.board = [[0, 1, 1, 0, 0]]
         point = self.ai.get_defending_point(board, (0, 1))
         self.assertEqual(None, point)
+
+
+class TestIsDoubleIntersection(unittest.TestCase):
+    def setUp(self):
+        self.ai = DummyAI(2, 1, "Black")
+
+    def test_double_two(self):
+        board = Board(5, 7)
+        board.board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0]
+        ]
+        actual = self.ai.is_double_intersection(board, 1, 3)
+        self.assertTrue(actual)
+
+    def test_double_two_with_gap(self):
+        board = Board(6, 9)
+        board.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+        actual = self.ai.is_double_intersection(board, 1, 4)
+        self.assertTrue(actual)
+
+    def test_double_dead_three(self):
+        board = Board(6, 9)
+        board.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 2]
+        ]
+        actual = self.ai.is_double_intersection(board, 1, 4)
+        self.assertTrue(actual)
+
+    def test_failed_case(self):
+        board = Board(6, 9)
+        board.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1]
+        ]
+        actual = self.ai.is_double_intersection(board, 1, 4)
+        self.assertFalse(actual)
+
+
+class TestHasDoubleIntersection(unittest.TestCase):
+    def setUp(self):
+        self.ai = DummyAI(2, 1, "Black")
+
+    def test_double_two(self):
+        board = Board(5, 7)
+        board.board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0]
+        ]
+        actual = self.ai.get_double_intersection(board)
+        self.assertEqual((1, 3), actual)
+
+    def test_double_two_with_gap(self):
+        board = Board(6, 9)
+        board.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+        actual = self.ai.get_double_intersection(board)
+        self.assertEqual((1, 4), actual)
+
+    def test_double_dead_three(self):
+        board = Board(6, 9)
+        board.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 2]
+        ]
+        actual = self.ai.get_double_intersection(board)
+        self.assertEqual((1, 4), actual)
+
+    def test(self):
+        board = Board(9, 9)
+        board.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+
+    def test_failed_case(self):
+        board = Board(6, 9)
+        board.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1]
+        ]
+        actual = self.ai.get_double_intersection(board)
+        self.assertEqual(None, actual)
