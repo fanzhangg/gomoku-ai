b0e2e4a1c295c3c3267b4b1b1ca67ccd96b0520f
fanzhangg <vanadiumzhang@gmail.com>
2019-12-18 13:09:21 -0600

    Update: gitignore

---
+++ test_scoreCounter.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestScoreCounter(TestCase):
+    def test_get_chains_at(self):
+        self.fail()
+
+    def test_update_scores(self):
+        self.fail()
+
+    def test_is_win(self):
+        self.fail()
+
+    def test_get_max_chain_at_step(self):
+        self.fail()
