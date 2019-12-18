205fbbc42593175d48c4a5c1c8cab1fb4c2817c9
fanzhangg <vanadiumzhang@gmail.com>
2019-12-18 13:12:27 -0600

    ZF vs Black AI

--- ai_competition.py
+++ ai_competition.py
@@ -4,17 +4,17 @@ from classes import *
                 
 if __name__ == "__main__":
     Player_lv = Player_LV(1, "Black")
-    Player_zf = DummyAI(2, "White")
+    Player_zf = DummyAI(2, 1, "White")
 
     Player_black = Player(1, "Black")
     Player_white = Player(2, "White")
 
     # Can choose different modes
-    game = Game(Board(15, 15), Player_lv, Player_zf)
+    # game = Game(Board(15, 15), Player_lv, Player_zf)
     # game = Game(Board(15, 15), Player_zf, Player_lv)
     # game = Game(Board(15, 15), Player_lv, Player_white)
     # game = Game(Board(15, 15), Player_black, Player_lv)
     # game = Game(Board(15, 15), Player_zf, Player_black)
-    # game = Game(Board(15, 15), Player_black, Player_zf)
+    game = Game(Board(15, 15), Player_black, Player_zf)
 
     game.play_one_round()
